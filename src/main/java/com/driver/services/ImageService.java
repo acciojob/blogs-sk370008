17d7e8b3b27369016175d893d07853fc0efc13ca
Shoaib Akhtar <sk370008@gmail.com>
2023-04-10 13:27:49 +0530

    check

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -1,3 +1,52 @@
+//package com.driver.services;
+//
+//import com.driver.models.*;
+//import com.driver.repositories.*;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public class ImageService {
+//
+//    @Autowired
+//    BlogRepository blogRepository2;
+//    @Autowired
+//    ImageRepository imageRepository2;
+//
+//    public Image addImage(Integer blogId, String description, String dimensions){
+//        //add an image to the blog
+//        Blog blog = blogRepository2.findById(blogId).get();
+//        Image image = new Image(description,dimensions,blog);
+//        blog.getImageList().add(image);
+//        blogRepository2.save(blog);
+//        return image;
+//    }
+//
+//
+//    public int countImagesInScreen(Integer id, String screenDimensions) {
+//        //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+//        String[] screeDimensionArr = screenDimensions.split("X");
+//
+//
+//        Image image = imageRepository2.findById(id).get();
+//
+//        String imageDimensions = image.getDimensions();
+//        String[] imageDimensionArr = imageDimensions.split("X");
+//        int length = Integer.parseInt(screeDimensionArr[0])/Integer.parseInt(imageDimensionArr[0]);
+//        int breadth = Integer.parseInt(screeDimensionArr[1]) / Integer.parseInt(imageDimensionArr[1]);
+//        int noOfImages = length * breadth;
+//        return noOfImages;
+//    }
+//
+//
+//
+//    public void deleteImage(Integer id){
+//        imageRepository2.deleteById(id);
+//    }
+//}
+
 package com.driver.services;
 
 import com.driver.models.*;
@@ -15,34 +64,47 @@ public class ImageService {
     @Autowired
     ImageRepository imageRepository2;
 
-    public Image addImage(Integer blogId, String description, String dimensions){
+    public Image addImage(Integer blogId, String description, String dimensions) {
         //add an image to the blog
+//        if(!blogRepository2.findById(blogId).isPresent()) {
+//            throw new Exception();
+//        }
         Blog blog = blogRepository2.findById(blogId).get();
         Image image = new Image(description,dimensions,blog);
         blog.getImageList().add(image);
         blogRepository2.save(blog);
         return image;
+        //Here I am not explicitly adding image in image-repository because due to cascading effect
     }
-
-
-    public int countImagesInScreen(Integer id, String screenDimensions) {
-        //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
-        String[] screeDimensionArr = screenDimensions.split("X");
-
-
-        Image image = imageRepository2.findById(id).get();
-
-        String imageDimensions = image.getDimensions();
-        String[] imageDimensionArr = imageDimensions.split("X");
-        int length = Integer.parseInt(screeDimensionArr[0])/Integer.parseInt(imageDimensionArr[0]);
-        int breadth = Integer.parseInt(screeDimensionArr[1]) / Integer.parseInt(imageDimensionArr[1]);
-        int noOfImages = length * breadth;
-        return noOfImages;
-    }
-
-
 
     public void deleteImage(Integer id){
         imageRepository2.deleteById(id);
+    }
+
+    public int countImagesInScreen(Integer id, String screenDimensions) {
+        //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        String [] scrarray = screenDimensions.split("X"); //A=Length   X    B=Breadth
+//        if(!imageRepository2.findById(id).isPresent()){
+//            throw new Exception();
+//        }
+        Image image = imageRepository2.findById(id).get();
+
+        String imageDimensions = image.getDimensions();
+        String [] imgarray = imageDimensions.split("X");
+
+        int scrl = Integer.parseInt(scrarray[0]); //A -- > integer
+        int scrb = Integer.parseInt(scrarray[1]); //B -- > integer
+
+        int imgl = Integer.parseInt(imgarray[0]); //A -- > integer
+        int imgb = Integer.parseInt(imgarray[1]); //B -- > integer
+
+        return no_Images(scrl,scrb,imgl,imgb);
+
+    }
+
+    private int no_Images(int scrl, int scrb, int imgl, int imgb) {
+        int lenC = scrl/imgl; //
+        int lenB = scrb/imgb;
+        return lenC*lenB;
     }
 }
